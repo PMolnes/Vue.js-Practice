22056602db0e56caddb2ae9957c90fb54aeba211
Petter Molnes <petter.molnes@solwr.com>
2022-06-29 14:01:40 +0200

    Finished section about routing

--- basics/routing-01-starting-setup/src/main.js
+++ basics/routing-01-starting-setup/src/main.js
@@ -1,20 +1,9 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
 
+import router from './router.js';
 import App from './App.vue';
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/teams', component: TeamsList },
-    { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers }
-  ],
-  linkActiveClass: 'active'
-});
+
 
 const app = createApp(App);
 app.use(router);
---
+++ basics/routing-01-starting-setup/src/router.js
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './pages/TeamsList.vue';
+import UsersList from './pages/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './pages/NotFound.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true},
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    },
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      beforeEnter(to, from, next) {
+        next();
+      },
+    },
+
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(_, _2, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(to, from);
+  next();
+});
+
+router.afterEach((to, from) => {
+  // sending analytics data
+  console.log(to, from);
+});
+
+export default router;
